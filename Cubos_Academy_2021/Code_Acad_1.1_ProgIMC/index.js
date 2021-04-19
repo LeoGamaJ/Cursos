1daaaa08e1f76d2192c7321258fbc15faae01d8d
Leo Gama J <53717882+LeoGamaJ@users.noreply.github.com>
2021-04-19 11:21:03 -0300

    Add files via upload

---
+++ Cubos_Academy_2021/Code_Acad_1.1_ProgIMC/index.js
@@ -0,0 +1,15 @@
+// IMC
+// imc = massa(kg)/altura_ao_quadrado(em m)
+
+const massa = 86;
+const altura = 1.90;
+
+const imc = massa / (altura * altura);
+
+console.log(imc);
+
+// Como melhorar esse código:
+// Perguntar ao usuário o nome, o peso e a altura dele
+// Fazer o calculo a partir da entrada de dados
+// -if/else- enquadrálo numa faixa de peso (pesuisar)
+// Responder ao usuário de forma extensa 

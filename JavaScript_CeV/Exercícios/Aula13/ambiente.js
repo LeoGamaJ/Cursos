dcd99ca7378295b46777293efd0c45becaf564c9
Leo Gama J <53717882+LeoGamaJ@users.noreply.github.com>
2021-04-27 16:16:45 -0300

    Add files via upload

---
+++ JavaScript_CeV/Exercícios/Aula13/ambiente.js
@@ -0,0 +1,16 @@
+// Estrutura de repetição com teste lógico e início. 
+
+var c = 1
+while (c < 7) {
+    console.log(`Passo ${c}`)
+    c ++
+}
+
+// Estrutura de repetição com teste lógico no final
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c ++
+} while (c < 7)
+

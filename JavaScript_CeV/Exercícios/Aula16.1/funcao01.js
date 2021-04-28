d7183b676e3496496a56e16b2f601c4a66c0a94e
Leo Gama J <53717882+LeoGamaJ@users.noreply.github.com>
2021-04-28 13:13:59 -0300

    Add files via upload

---
+++ JavaScript_CeV/Exercícios/Aula16.1/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return `O número ${n} é PAR!` 
+    } else {
+        return `O número ${n} é ÍMPAR!`
+    }
+}
+
+/*
+let res = parimpar(4)
+console.log(res)
+*/
+console.log(parimpar(13))
